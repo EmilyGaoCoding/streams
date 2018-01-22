d1857c82b9ad64a4a3701c1c1e2f9d1bea58d94c
Song Gao <song.d.gao@gmail.com>
2018-01-22 21:32:06 +0000

    this module can print with url as parameters

---
+++ step-3-print-with-parameters.js
@@ -0,0 +1,23 @@
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+  var https = require('https');
+  var output = '';
+
+  https.get(options, function (response) {
+    response.on('data', function (data) {
+      output += data;
+    });
+    response.on('end', function () {
+      console.log(output);
+    });
+  })
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
